288249dd14ebd0fa2c804159152bb5b6d296e45b
Marlon5700 <marlonthompson5700@gmail.com>
2025-11-04 05:05:23 +0000

    Website Fix.
    Fixed an issue with the sign up button flickering upon switching between web and mobile view.

--- app.js
+++ app.js
@@ -5,4 +5,16 @@ const menuLinks = document.querySelector('.navbar__menu')
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
+      
 })
+
+let resizeTimer;
+window.addEventListener('resize', () => {
+  document.body.classList.add('resizing');
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove('resizing');
+  }, 250);
+});
+
+
